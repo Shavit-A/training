daff215a1f7d96209c5b2244fba766fbf1d720ae
Shavit A <177243351+Shavit-A@users.noreply.github.com>
2026-03-04 13:46:36 +0200

    Implement Echo server

--- server.c
+++ server.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
@@ -9,6 +10,9 @@
 #include <netinet/in.h>
 
 #define PORT (12345)
+#define NUMBER_OF_CLIENTS (3)
+#define BUFFER_SIZE (1024)
+#define QUIT_CMD "quit"
 
 #define VALIDATE(condition) do { \
     if (!(condition)) { \
@@ -24,13 +28,13 @@ typedef enum Status_s {
     FAILURE = -1
 } Status;
 
-int main(int argc, char const* argv[])
+int main(int argc, char const* const argv[])
 {
     Status status = FAILURE;
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     VALIDATE(server_fd != FAILURE);
 
-    int opt = 1;
+    const int opt = 1;
     status = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
     VALIDATE(status == SUCCESS);
 
@@ -42,7 +46,7 @@ int main(int argc, char const* argv[])
     status = bind(server_fd, (struct sockaddr*)&address, sizeof(address));
     VALIDATE(status == SUCCESS);
 
-    status = listen(server_fd, 3);
+    status = listen(server_fd, NUMBER_OF_CLIENTS);
     VALIDATE(status == SUCCESS);
 
     printf("Waiting for client connection on port %d\n", PORT);
@@ -52,11 +56,36 @@ int main(int argc, char const* argv[])
     int client_fd = accept(server_fd, (struct sockaddr*)&client_address, &address_len);
     VALIDATE(client_fd != FAILURE);
 
-    send(client_fd, "Hello from server", 17, 0);
+    printf("Client connected\n");
+
+    char buffer[BUFFER_SIZE] = {0};
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+        
+        ssize_t bytes_received = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+        VALIDATE(bytes_received >= 0);
+        if (bytes_received == 0) {
+            printf("Client closed connection\n");
+            break;
+        }
+        
+        printf("Received: %s\n", buffer);
+        
+        const char* const quit_cmd = QUIT_CMD;
+        if (strncmp(buffer, quit_cmd, strlen(quit_cmd)) == 0) {
+            printf("Client requested to close connection\n");
+            break;
+        }
+        
+        ssize_t bytes_sent = send(client_fd, buffer, bytes_received, 0);
+        VALIDATE(bytes_sent == bytes_received);
+        
+        printf("Echoed %ld bytes back to client\n", bytes_sent);
+    }
 
     close(client_fd);
     close(server_fd);
 
-    printf("Hello message sent\n");
+    printf("Connection closed\n");
     return EXIT_SUCCESS;
 }
