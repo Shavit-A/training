f8251e8f732257f09def03d572fe8538ba72714b
Shavit A <shavit100amit@gmail.com>
2026-03-04 13:32:55 +0200

    Server - send simple message to client and finish

--- server.c
+++ server.c
@@ -1,7 +1,62 @@
-#include "stdio.h"
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT (12345)
+
+#define VALIDATE(condition) do { \
+    if (!(condition)) { \
+        fprintf(stderr, "Validation failed at %s:%d\n", __FILE__, __LINE__); \
+        fprintf(stderr, "Condition: %s\n", #condition); \
+        fprintf(stderr, "Error [%d]: %s\n", errno, strerror(errno)); \
+        return EXIT_FAILURE; \
+    } \
+} while (0)
+
+typedef enum Status_s {
+    SUCCESS = 0,
+    FAILURE = -1
+} Status;
 
 int main(int argc, char const* argv[])
 {
-    printf("Hello world\n");
-    return 0;
+    Status status = FAILURE;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    VALIDATE(server_fd != FAILURE);
+
+    int opt = 1;
+    status = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+    VALIDATE(status == SUCCESS);
+
+    struct sockaddr_in address = {0};
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    status = bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    VALIDATE(status == SUCCESS);
+
+    status = listen(server_fd, 3);
+    VALIDATE(status == SUCCESS);
+
+    printf("Waiting for client connection on port %d\n", PORT);
+
+    struct sockaddr_in client_address = {0};
+    socklen_t address_len = sizeof(client_address);
+    int client_fd = accept(server_fd, (struct sockaddr*)&client_address, &address_len);
+    VALIDATE(client_fd != FAILURE);
+
+    send(client_fd, "Hello from server", 17, 0);
+
+    close(client_fd);
+    close(server_fd);
+
+    printf("Hello message sent\n");
+    return EXIT_SUCCESS;
 }
